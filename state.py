7e641161ccc4b3e6f9cd199f4b1b6427866fea64
Hans Yu <hans.yu@outlook.de>
2022-01-04 16:44:13 +0100

    Game class.

--- state.py
+++ state.py
@@ -12,10 +12,10 @@ class Grid:
     def __init__(self):
         self.state: np.ndarray = np.full((3, 3), Move.NONE)
 
-    def set_player_1(self, player: Player):
+    def set_player_1(self, player: "Player"):
         player.turn: Move = Move.ONE
 
-    def set_player_2(self, player: Player):
+    def set_player_2(self, player: "Player"):
         player.turn: Move = Move.TWO
 
     def move(self, turn: Move, row: int, col: int):
@@ -25,13 +25,33 @@ class Grid:
         self.state[row, col] = turn
 
 class Player:
-    def set_grid(self, grid: Grid):
+    def __init__(self):
+        self.turn = None
+
+    def set_grid(self, grid: Grid, turn: Move = Move.NONE):
         self.grid: Grid = grid
+
+        if turn == Move.ONE:
+            self.grid.set_player_1(self)
+        elif turn == Move.TWO:
+            self.grid.set_player_2(self)
 
     def move(self, row: int, col: int):
         self.grid.move(self.turn, row, col)
 
+class Game:
+    def __init__(self):
+        self.grid = Grid()
+        self.player_1 = Player()
+        self.player_2 = Player()
+
+        self.player_1.set_grid(self.grid, Move.ONE)
+        self.player_2.set_grid(self.grid, Move.TWO)
+
 if __name__ == "__main__":
-    grid = Grid()
-    print(grid.state)
+    game = Game()
+    print(game.grid.state)
+
+    game.player_1.move(1, 1)
+    print(game.grid.state)
 
